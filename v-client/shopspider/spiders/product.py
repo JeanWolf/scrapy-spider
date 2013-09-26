55413ecb29aee4aaddce9ef590db4c1ff7cd8d49
wj922 <wj922@qq.com>
2013-09-26 11:55:05 +0800

    remove some useless files

--- v-client/shopspider/spiders/product.py
+++ v-client/shopspider/spiders/product.py
@@ -2,15 +2,15 @@
 import sys,re,urllib2,os,socket
 reload(sys)
 sys.setdefaultencoding('utf8')
-socket.setdefaulttimeout(30)
 
 from scrapy.conf import settings
 from scrapy.http import Request,HtmlResponse
 from scrapy.selector import HtmlXPathSelector
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.contrib.spiders import CrawlSpider, Rule
-from shopspider.diy.funs import replace_item,init_item,handle_item #载入自定义方法
+from shopspider.diy.jeen import Jeen #载入自定义类方法
 from shopspider.diy.configs import *
+socket.setdefaulttimeout(conf_time_out)
 
 class ProductSpider(CrawlSpider): #单独抓产品
     name = 'product'
@@ -30,23 +30,6 @@ class ProductSpider(CrawlSpider): #单独抓产品
 	#分析代码中请将页面中分析得到的图片路径赋值到新字段  logo_src  photo_src  image_src 中
 #-- parse_product Start-----------------------------------------------
     def parse_product(self, response):
-        print '\r\n\t======== Page Crawl Start - Product -----------'
-        hxs = HtmlXPathSelector(response)
-        item = init_item('product') #初始化 shop item
-        try :
-            if conf['show_messages'] : print '----Product Fetch Start----'
-        #--分析代码开始#################################################################################################################
-            item['purl'] = response.url
-        #--分析代码结束#################################################################################################################
-            if conf['show_messages'] : print '---- Fetch Success ----'
-        except EOFError,e :
-            if conf['show_messages'] : print '----Product Fetch Error Start----'
-            print e
-            if conf['show_messages'] : print '----Product Fetch Error End----'
-        if conf['show_messages'] : print '----Product Fetch End----'
-        item = handle_item(item) #处理item 主要处理一些特殊字符  换行符  及多余空格
-        global p_count; p_count += 1;
-        print str(p_count)+' Product\t======== Page Crawl Information End ========================='
-        del hxs,response #销毁相关变量
-        return item
-#---Parse_product End---------------------------------------------------------
+        #请参考 all.py 中的产品分析部分
+        pass
+#-- parse_product End  -----------------------------------------------
--- v-client/shopspider/spiders/shop.py
+++ v-client/shopspider/spiders/shop.py
@@ -2,15 +2,15 @@
 import sys,re,urllib2,os,socket
 reload(sys)
 sys.setdefaultencoding('utf8')
-socket.setdefaulttimeout(30)
 
 from scrapy.conf import settings
 from scrapy.http import Request,HtmlResponse
 from scrapy.selector import HtmlXPathSelector
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.contrib.spiders import CrawlSpider, Rule
-from shopspider.diy.funs import replace_item,init_item,handle_item #载入自定义方法
+from shopspider.diy.jeen import Jeen #载入自定义类方法
 from shopspider.diy.configs import *
+socket.setdefaulttimeout(conf_time_out)
 
 class CompanySpider(CrawlSpider): #单独抓商家
     name = 'shop'
@@ -30,23 +30,6 @@ class CompanySpider(CrawlSpider): #单独抓商家
 	#分析代码中请将页面中分析得到的图片路径赋值到新字段  logo_src  photo_src  image_src 中
 #-- parse_shop Start-----------------------------------------------
     def parse_shop(self, response):
-        print '\r\n\t======== Page Crawl Start - Company -----------'
-        hxs = HtmlXPathSelector(response)
-        item = init_item('shop') #初始化 shop item
-        try :
-            if conf['show_messages'] : print '----Company Fetch Start----'
-        #--分析代码开始#################################################################################################################
-            item['url'] = response.url
-        #--分析代码结束#################################################################################################################
-            if conf['show_messages'] : print '---- Fetch Success ----'
-        except EOFError,e :
-            if conf['show_messages'] : print '----Company Fetch Error Start----'
-            print e
-            if conf['show_messages'] : print '----Company Fetch Error End----'
-        if conf['show_messages'] : print '----Company Fetch End----'
-        item = handle_item(item) #处理item 主要处理一些特殊字符  换行符  及多余空格
-        global s_count ; s_count += 1;
-        print str(s_count)+' Company\t======== Page Crawl Information End ========================='
-        del hxs,response #销毁相关变量
-        return item
+        #请参考 all.py 中的商家分析部分
+        pass
 #---Parse_shop End---------------------------------------------------------
